71344ed1c3f0ee7edb85f45c5eb2a2b4976b7575
Abhishek Amralkar <abhishekamralkar@gmail.com>
2023-11-01 19:10:39 -0700

    [gowc] add count logic

--- main.go
+++ main.go
@@ -1,9 +1,41 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
 )
 
+func wordCount(filename string) int {
+	f, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("unable to readfile: %v", err)
+	}
+
+	defer f.Close()
+
+	fScanner := bufio.NewScanner(f)
+
+	fScanner.Split(bufio.ScanWords)
+
+	count := 0
+
+	for fScanner.Scan() {
+		count++
+	}
+
+	if err := fScanner.Err(); err != nil {
+		panic(err)
+	}
+
+	return count
+}
+
 func main() {
-	fmt.Println("Hello World!")
+	filepath := os.Args[1]
+	if len(os.Args) < 2 {
+		fmt.Println("Program accepts 2")
+	}
+	fmt.Println(wordCount(filepath))
 }
